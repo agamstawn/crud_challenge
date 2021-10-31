610692016560761993e056a7ac85142fa25cde57
agamstawn <agam.setiawan@nutrifood.co.id>
2021-10-31 20:46:48 +0700

    seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ name: 'User X', email: 'x@gmail.com' }, { name: 'User Y', email: 'y@gmail.com'}])
+
+neighborhood1 = Neighborhood.create(name: 'Neighborhood 1', coordinate_range: 'A-B')
+neighborhood2 = Neighborhood.create(name: 'Neighborhood 2', coordinate_range: 'A-B')
+
+flats = Flat.create([{ name: 'User X', neighborhood: neighborhood1, location: 'A1' }, { name: 'User Y', neighborhood: neighborhood2, location: 'A2'}])
